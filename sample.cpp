9cb491dfaee5a541cc8e882ca6f46481cbeea4af
avishkaindunil <haavishkaindunil@gmail.com>
2025-03-16 21:44:14 +0530

    update files

--- sample.cpp
+++ sample.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#define LOG(x) std::cout << x << std::endl;
 using namespace std;
 
 int main () {
     cout <<"Hello-World" << endl;
+    LOG("Hell0 World!");
     return 0;
 }
